96a5fe297d33fd713593cb83a322f1e226555d03
Carlos Vigil <carlos.vigil@icloud.com>
2017-09-16 16:50:26 -0400

    finish Py assignment 2

--- csc-212/assignments/Assignment2.py
+++ csc-212/assignments/Assignment2.py
@@ -10,7 +10,11 @@ def censor_program():
     """Opens an existing file and returns a new one with all 4 letter words
     censored by '****'.
     """
-    fout = open('alice_censor.txt', 'w')
+    try:
+        fout = open('alice_censor.txt', 'w')
+    except:
+        print('Error on opening file to write.')
+
     with open('alice.txt') as fin:
         for line in fin:
             for word in line.split():
@@ -23,10 +27,46 @@ def censor_program():
     # print(open('alice_censor.txt').read())
 
 
+def histogram(text):
+    """Receives text, outputs the occurance of each english letter."""
+    letters = {}
+
+    for char in text:
+        if char in letters:
+            letters[char] += 1
+        else:
+            letters[char] = 1
+    return letters
+
+
+def note():
+    """Compares two character dictionaries, returning false if the latter has
+    less of one char present in the former.
+    """
+    nicety = histogram(input('Hey there, type and enter a message here.\n'))
+    magazine = histogram(input('Now, enter the text from a magazine.\n'))
+    status = True
+
+    for key in nicety:
+        if key not in magazine:
+            status = False
+            break
+        elif nicety[key] > magazine[key]:
+            status = False
+            break
+    print(status)
+    return status
+
+
 def main():
     """Run all programs."""
     creds('September 16, 2016')
+    print('Assignment 2 - Exercise 1:\n')
     censor_program()
+    print('censor_program() has executed.')
+    print('Read alice_censor, i.e., `less alice_censor.txt`.')
+    print('\n- Exercise 2:\n')
+    note()
 
 if __name__ == '__main__':
     main()
