acc35113b747af8edd239494665e395df8cc0bfc
Carlos Vigil <carlos.vigil@icloud.com>
2017-10-03 01:15:10 -0400

    Finish hamburger class

--- csc-212/assignments/Assignment3.py
+++ csc-212/assignments/Assignment3.py
@@ -1,28 +1,51 @@
 """Hi"""
 
 
+def y_or_n():
+    """Accept a yes or no answer, then, return a corresponding boolean."""
+    answer = ''
+    yup = answer == 'yes' or answer == 'y'
+    nope = answer == 'no' or answer == 'n'
+
+    while not yup or not nope:
+        answer = input("Type 'yes' or 'no': ").lower()
+        yup = answer == 'yes' or answer == 'y'
+        nope = answer == 'no' or answer == 'n'
+
+        if yup:
+            return True
+        elif nope:
+            return False
+        else:
+            print('Error, please try again.')
+
+
 class Hamburger:
     """The order slip at your local burger spot."""
     def __init__(self, weight=8, done='well', cheese=False, topp='lettuce'):
-        self._weight = weight
+        self._weight = int(weight)
         self._doneness = done
         self._cheese = cheese
         # Toppings will be a list if user customizes their order
         self._toppings = topp
 
     def __str__(self):
-        return str(vars(self))
+        return ('{0._weight} oz\n'
+                'Cooked {0._doneness}\n'
+                'Cheese: {0._cheese}\n'
+                'Toppings: {0._toppings}').format(self)
 
     def set_weight(self, weight):
-        """Mutate the _weight attribute of the given instance."""
-        self._weight = weight
+        """Mutate the _weight attribute of the given instance, converting
+        the input into an interger.
+        """
+        self._weight = int(weight)
 
     def get_weight(self):
         """Access the _weight attribute of the given instance, returning
         a string with ' oz' appended.
         """
-        # TODO: TEST
-        return str(self._weight + ' oz')
+        return self._weight
 
     def set_doneness(self, cook):
         """Mutate the _doneness attribute of the given instance."""
@@ -56,60 +79,64 @@ class Hamburger:
         """Lessen the weight of the instantiated burger by one ounce."""
         if self._weight > 0:
             self._weight -= 1
+            return True
+        return False
 
 
-def ask_for_burger():
+def eat(order):
+    """Lower the weight of the burger until it is gone."""
+    print('Would you like to eat it?')
+    if y_or_n():
+        while order.bite():
+            print('{} oz'.format(order.get_weight()))
+    else:
+        print('OK.')
+
+    print('Would you like to order another burger?')
+    return order_burger() if y_or_n() else print('Have a great day!')
+
+
+def order_burger():
     """Aquire user input, decide how to instantiate a Hamburger class."""
     print('Welcome to Burger Place!')
     print('Would you like to customize your burger?')
 
-    if yes_no_input():
+    if y_or_n():
         print('How large would you like your burger?')
         ounces = input('Enter it in ounces: ')
         print('Great, thanks! Now, how well would you like your burger cooked?')
         cook_it = input('Enter here: ')
         print('OK. What about cheese?')
-        cheese = yes_no_input()
+        cheese = y_or_n()
         print('Alright, now what toppings do you want?')
         topp = input('Enter each topping seperated by a space: ')
 
         custom_order = Hamburger(ounces, cook_it, cheese, topp)
+        print('\nYour custom order...')
         print(custom_order)
-        return custom_order
-
-    print('\nOK! One classic burger')
-    return Hamburger()
-
-
-def yes_no_input():
-    """Accept a yes or no answer, then, return a corresponding boolean."""
-    answer = ''
-    yup = answer == 'yes' or answer == 'y'
-    nope = answer == 'no' or answer == 'n'
-
-    while not yup or not nope:
-        answer = input("Type 'yes' or 'no': ").lower()
-        yup = answer == 'yes' or answer == 'y'
-        nope = answer == 'no' or answer == 'n'
-
-        if yup:
-            return True
-        elif nope:
-            return False
-        else:
-            print('Error, please try again.')
+        print()
+        eat(custom_order)
+    else:
+        print('\nOK! One classic burger!')
+        classic_order = Hamburger()
+        print(classic_order)
+        print()
+        eat(classic_order)
 
 
 def creds(day):
     """seal of origin; take day, output an assignment header."""
-    print('\n# Carlos Vigil\n# A. Abd El-Raouf\n# CSC 212-02\n# %s\n' % (day))
+    print('\n# Carlos Vigil'
+          '\n# A. Abd El-Raouf'
+          '\n# CSC 212-02'
+          '\n# {}\n'.format(day))
 
 
 def main():
     """Runs the defined functions or methods."""
-    creds('October 1, 2017')
-    order = ask_for_burger()
-    print(order)
+    creds('October 2, 2017')
+    order_burger()
+
 
 if __name__ == '__main__':
     main()
